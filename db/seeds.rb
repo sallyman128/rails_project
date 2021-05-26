19d8b4b1f9246601ec9117a05653020217e7d808
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-26 14:54:33 -0400

    create seed data

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# create users
+user_names = ["riku", "sora", "kairi", "cloud"]
+user_names.each do |user_name|
+  User.create(name: user_name, email: "#{user_name}@kh.com", password: "#{user_name}pass")
+end
+
+# create carts and associate them to the user
+for User.all.each do |user|
+  3.times { |i| user.carts.create(name: "#{user.name}'s wish list ##{i}") }
+end
+
+#create items
+items = {
+  {name: ""},
+  orange: ,
+  apple: ,
+  nintendo_switch: ,
+  ,
+}
+for items
+
+
