8513a03b4ae07cd11d089d6235a1cde23b977891
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-27 17:32:01 -0400

    user can add new paintings

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -6,4 +6,22 @@ class PaintingsController < ApplicationController
   def show
     @painting = Painting.find_by(id: params[:id])
   end
+
+  def new
+    @painting = Painting.new
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+    if @painting.save
+      redirect_to paintings_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def painting_params
+    params.require(:painting).permit(:title, :artist_name, :country)
+  end
 end
