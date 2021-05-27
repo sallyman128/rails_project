e9bc964cf80a7134adc30aee1c6519cc4892bb62
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-27 05:32:00 -0400

    redirect user to paintings index after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,13 @@
 class SessionsController < ApplicationController
   def signin #get
-
   end
 
   def login #post
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to 
+      redirect_to paintings_path
     else
-
       redirect_to signin_path
     end
   end
