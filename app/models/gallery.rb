85cb4f163c741128849a022e5f69aff8c3707d10
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-27 04:31:52 -0400

    add validations to models

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,6 @@
 class Gallery < ApplicationRecord
+  validates :name, presence: true
+
   belongs_to :user
   has_many :gallery_paintings
   has_many :paintings, through: :gallery_paintings
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,8 @@
 class Painting < ApplicationRecord
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :country, presence: true
+
   has_many :gallery_paintings
   has_many :galleries, through: :gallery_paintings
   has_many :users, through: :galleries
